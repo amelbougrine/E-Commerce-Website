992ca80a955d408b143f228dae099133fcbbfd7f
Amelie121212 <amel.1989ab@gmail.com>
2020-06-29 23:57:25 +0100

    Fetch Server Data (React Hook)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import data from './data';
 import express from 'express';
+import data from './data';
 
 const app = express();
-app.get("/api/products",(req, res) => {
+app.get("/api/products", (req, res) => {
     res.send(data.products);
 })
 app.listen(5000, () => {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function HomeScreen (props) {
+  //define a Hook
+  const [products, setProduct] = useState({});
+  //fetch data from server
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/products");
+      setProduct(data);
+    }
+    return () => {
+      //
+    };
+  }, [])
     return (
     <ul className="products">
-        {data.products.map(product =>
+        {products.map(product =>
         <li>
           <div className="product">
             <Link to={"/product/" + product.id}>
