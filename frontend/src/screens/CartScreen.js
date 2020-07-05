11f285a2e498a058b331a6e217dca338d583228b
Amelie121212 <amel.1989ab@gmail.com>
2020-07-05 23:59:46 +0100

    Sopping Cart Screen 02

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,6 +3,8 @@ import {useDispatch} from 'react-redux';
 import {addToCart} from '../actions/cartActions';
 
 function CartScreen (props) {
+    const cart = useSelector(state =>state.cart);
+    const {cartItems} = cart;
     const productId = props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
     const dispatch = useDispatch();
@@ -11,6 +13,33 @@ function CartScreen (props) {
         dispatch(addToCart(productId, qty));
         }
     }, [])
-    return <div> Cart Screen </div>
+    return (
+    <div className="cart">
+        <div className="cart-list">
+            <ul className="cart-list-container">
+                <li>
+                    <h3>Shopping Cart</h3>
+                    <div>Price</div>
+                </li>
+                {cartItems.length === 0 ? <div> Cart is Empty.</div> :
+                    cartItems.map( item =>
+                        <div>
+                            <img src={item.image} alt="product" />
+                            <div className="cart-name">
+                                <div>{item.name}</div>
+                                <div>Qty: <select>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                </select></div>
+                            </div>
+                            <div>{item.price}</div>
+                        </div>
+                    )
+                }
+            </ul>
+        </div>
+    </div>
+    )
 }
 export default CartScreen;
