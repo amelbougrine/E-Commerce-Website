027f49f061c11b2ccb70e389854e45fbb12e1e10
Amelie121212 <amel.1989ab@gmail.com>
2020-07-08 23:07:38 +0100

    Sign In User (JWT)

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/eshop'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/eshop',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import User from '../models/userModel'
+import { getToken } from '../util';
 
 const router = express.Router();
-
+router.post("/signin", async (req, res) => {
+    const singninUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if (signinUser) {
+        res.send({
+            id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(user)
+        })
+    } else {
+        res.status(401).send({msg: "Invalid Email or Password"});
+    }
+})
 router.get("/createadmin", async (req, res) => {
     try {
         const user = new User({
