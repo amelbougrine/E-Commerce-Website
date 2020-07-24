7b1c83747c27535584f2b431477d96c532060aaa
Amelie121212 <amel.1989ab@gmail.com>
2020-07-24 23:57:26 +0100

    Fix a bug

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,18 @@ import config from './config';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
-import bodyparser from 'body-parser';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  //useCreateIndexes: true
-}).catch(error => console.log(error.reason));
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }).catch((error) => console.log(error.reason));
 
 const app = express();
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 app.use('/api/users', userRoute);
 app.get('/api/products', (req, res) => {
   res.send(data.products);
@@ -27,6 +27,7 @@ app.get('/api/products/:id', (req, res) => {
     res.send(product);
   } else res.status(404).send({msg: "product Not Found"});
 });
+
 app.listen(5000, () => {
   console.log("server started at http://localhost:5000");
 });
